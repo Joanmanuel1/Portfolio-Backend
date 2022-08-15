b5dc0171c834e88d54f22a14ec2fdcced10d45eb
Joan Manuel Romero <40809044+Joanmanuel1@users.noreply.github.com>
2022-08-15 01:28:23 -0300

    falla deploy automatico heroku

--- src/main/java/com/example/demo/controller/Controlador.java
+++ src/main/java/com/example/demo/controller/Controlador.java
@@ -40,10 +40,10 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  */
 @RestController
 
-@CrossOrigin  ACCEDEN TODOS
+//@CrossOrigin  ACCEDEN TODOS
 //@CrossOrigin(origins = "https://portfolio-front-end-eef7f.web.app") ACCEDE UNO SOLO 
-//@CrossOrigin(origins = {"http://localhost:4200", "https://portfolio-front-end-eef7f.web.app", "https://portfolio-arg-programa.herokuapp.com"}) // MULTIPLES CONEXIONES
-
+@CrossOrigin(origins = {"http://localhost:4200", "https://portfolio-front-end-eef7f.web.app", "https://portfolio-arg-programa.herokuapp.com"}) // MULTIPLES CONEXIONES
+                                                                                        
 public class Controlador {
 
     @Autowired
