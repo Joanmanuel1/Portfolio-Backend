54edb78129f7945bd871956adc14394c5489be94
Joan Manuel Romero <40809044+Joanmanuel1@users.noreply.github.com>
2022-08-21 02:43:10 -0300

    Update PersonaRepository.java

--- src/main/java/com/example/demo/repository/PersonaRepository.java
+++ src/main/java/com/example/demo/repository/PersonaRepository.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.example.demo.repository;
-
 import com.example.demo.model.Persona;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- *
- * @author usuario
- */
 public interface PersonaRepository extends JpaRepository <Persona, Long>{
     public boolean existsByEmail(String email);
+    public boolean existsByPassword(String password);
 }
