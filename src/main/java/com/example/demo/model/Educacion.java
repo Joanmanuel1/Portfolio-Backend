5d6d6ec3eb9e4bfd7fe82e0110671367dbd964ed
Joan Manuel Romero <40809044+Joanmanuel1@users.noreply.github.com>
2022-08-21 02:38:48 -0300

    Update Educacion.java

--- src/main/java/com/example/demo/model/Educacion.java
+++ src/main/java/com/example/demo/model/Educacion.java
@@ -4,6 +4,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter @Setter
 @Entity
