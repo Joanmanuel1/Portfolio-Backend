6b52124867ee8fac2fb27f925239c22fa75249eb
Joan Manuel Romero <40809044+Joanmanuel1@users.noreply.github.com>
2022-08-21 02:41:30 -0300

    Add files via upload

---
+++ src/main/java/com/example/demo/repository/IdiomaRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+import com.example.demo.model.Idioma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IdiomaRepository extends JpaRepository <Idioma, Long>{
+    
+}
