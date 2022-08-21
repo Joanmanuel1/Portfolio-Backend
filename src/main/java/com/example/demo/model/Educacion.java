1dab2afa3a516a85d56eb8ba5f7e7640076a96e9
Joan Manuel Romero <40809044+Joanmanuel1@users.noreply.github.com>
2022-08-21 02:37:37 -0300

    Update Educacion.java

--- src/main/java/com/example/demo/model/Educacion.java
+++ src/main/java/com/example/demo/model/Educacion.java
@@ -1,20 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.demo.model;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Getter;
-import lombok.Setter;
-
-/**
- *
- * @author usuario
- */
 
 @Getter @Setter
 @Entity
