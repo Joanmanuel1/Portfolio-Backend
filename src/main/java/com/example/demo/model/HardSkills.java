8b3a57402c6b951135ba7252bfc6514dd2a93795
Joan Manuel Romero <40809044+Joanmanuel1@users.noreply.github.com>
2022-08-21 02:39:40 -0300

    Update HardSkills.java

--- src/main/java/com/example/demo/model/HardSkills.java
+++ src/main/java/com/example/demo/model/HardSkills.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.demo.model;
 
 import javax.persistence.Entity;
@@ -10,11 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-
-/**
- *
- * @author usuario
- */
 
 @Getter @Setter
 @Entity
