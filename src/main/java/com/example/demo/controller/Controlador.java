5f87f3b4d6eef23caf7f1818935d7f6695504d1d
Joan Manuel Romero <40809044+Joanmanuel1@users.noreply.github.com>
2022-12-27 21:51:40 -0300

    request mapping
    agregamos linea

--- src/main/java/com/example/demo/controller/Controlador.java
+++ src/main/java/com/example/demo/controller/Controlador.java
@@ -17,7 +17,6 @@ import java.time.Instant;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,10 +26,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @RestController
+@RequestMapping("/")
 
 //ACCEDEN TODOS
  @CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
