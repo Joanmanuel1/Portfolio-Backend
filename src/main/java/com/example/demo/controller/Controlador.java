fbfd69e3a2b7afbf7837c5f9d1d05bf7e5a6a456
Joan Manuel Romero <40809044+Joanmanuel1@users.noreply.github.com>
2022-12-27 23:06:39 -0300

    Update Controlador.java

--- src/main/java/com/example/demo/controller/Controlador.java
+++ src/main/java/com/example/demo/controller/Controlador.java
@@ -17,6 +17,7 @@ import java.time.Instant;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
@@ -34,7 +36,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping("/")
 
 //ACCEDEN TODOS
- @CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
+@CrossOrigin(origins= "*", methods= {RequestMethod.GET,RequestMethod.POST})
 
 //ACCEDE UNO SOLO 
 //@CrossOrigin(origins = "https://portfolio-front-end-eef7f.web.app")
